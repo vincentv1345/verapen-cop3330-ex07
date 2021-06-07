9db67a0bd9f196849f7d873fb9e0512572f60862
Vincent <vincentverapen2012@gmail.com>
2021-06-06 21:00:01 -0400

    Final Commit

--- src/main/java/org/example/CalcArea.java
+++ src/main/java/org/example/CalcArea.java
@@ -1,7 +1,6 @@
 package org.example;
 public class CalcArea {
     public double calcArea(double a, double b){
-        double area = a * b;
-        return area;
+        return a * b;
     }
 }
--- src/main/java/org/example/meterConversion.java
+++ src/main/java/org/example/meterConversion.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class meterConversion extends CalcArea{
     public double meters(double a){
-        double metersSquare = a * 0.09290304;
-        return metersSquare;
+        return a * 0.09290304;
     }
 }
